e17adf94377ceb397bfcb3afc85082db01d7b357
dcervant <3668610+dcervantes@users.noreply.github.com>
2020-07-30 15:32:01 +0200

    Get working the type15 from reading the CAT line to insert the json in Mongodb. Also passing validation with schema

--- catastro_to_mongodb.py
+++ catastro_to_mongodb.py
@@ -5,6 +5,7 @@ from os.path import basename
 from re import match
 from sys import exit
 from pathlib import Path
+from jsonschema import validate, ValidationError, SchemaError
 
 import warnings
 import click
@@ -59,59 +60,89 @@ def cli(database, host, port, timeout, file):
     """Import a CAT FILE to MongoDB"""
 
     # Connect to MongoDB client
-    # click.echo("....................................")
-    # click.echo(f"Connecting to {host}:{port}")
-    # click.echo("....................................")
-    # print()
-    # mongo_client = MongoClient(host, port, serverSelectionTimeoutMS=timeout)
-    # check_connection(mongo_client, host, port)
+    click.echo("....................................")
+    click.echo(f"Connecting to {host}:{port}")
+    click.echo("....................................")
+    print()
+    mongo_client = MongoClient(host, port, serverSelectionTimeoutMS=timeout)
+    check_connection(mongo_client, host, port)
 
     # Create database and collections
-    # db = mongo_client[database]
-    # cinmuebles = db["Inmuebles"]
+    db = mongo_client[database]
+    cinmuebles = db["Inmuebles"]
     fill_char = click.style('=', fg='yellow')
     cat_file_lines = line_count(file)
     print(cat_file_lines)
     cat_file = open(file, "r")
+    # read file
+    json_schemas = {}
+    json_dict = {}
+    with open('./schemas/tipo15.json', 'r') as myfile:
+        json_schemas =  json.loads(myfile.read())
+
+    with open('./dict/tipo15.json', 'r') as myfile:
+        json_dict =  json.loads(myfile.read())
+
     with open(file, "r") as cat_file:
         with click.progressbar(cat_file, label='Loading...', fill_char=fill_char, length=cat_file_lines, show_percent=True, show_pos=True) as lines:
             for line in lines:
-                sleep(0.01)
-                # print(line)
-                # cinmuebles.insert_one(cat_to_json_inmuebles(line)) 
+                cinmuebles.insert_one(cat_to_json(line, json_schemas, json_dict)) 
 
     click.echo(click.style("Import complete!", fg="green"))
 
-def cat_to_json_inmuebles(line):
-    x = {
-    "_id": "1",
-    "name": "John",
-    "age": 30,
-    "city": "New York"
-    }
+def cat_to_json(line, json_schemas, json_dict):
 
-    return json.dumps(x)
+    json_dict_keys = get_simple_keys(line, json_dict)
+    validate_json(json_dict_keys, json_schemas)
 
-def get_field_value(line, val_format, ini_char, length):
+    return json_dict_keys
+
+def get_field_value(line, data):
+
+    val_format = data["val_format"] 
+    ini_char = int(data ["ini_char"]) - 1
+    length = int(data["length"])
+    end_char = ini_char + length
+    value = line[ini_char:end_char].strip()
     # Text format
     if val_format == 'X':
-        return line[ini_char:ini_char + val_format]
+        if "dict" in data:
+            return data["dict"][value]
+        return value
     
     elif val_format == 'N':
-        return int(line[ini_char:ini_char + val_format])
+        if "decimal" in data:
+            return float(value)/int(data["decimal"])
+        return int(value)
     else:
         return ""
 
 
-def process_line(line):
-    x = {
-    "_id": "1",
-    "name": "John",
-    "age": 30,
-    "city": "New York"
-    }
+def get_simple_keys(line, data):
+    result = {}
+    for pkey, pvalue in data.items():
+        result[pkey] = {}
+        for skey, svalue in pvalue.items():
+            value = get_field_value(line, svalue)
+            if value != "":
+                result[pkey][skey] = value
+    return result
 
-    return json.dumps(x)
+def validate_json(validJson, schema):
+    try:
+        validate(validJson, schema)
+    
+    except SchemaError as e:
+        print(e)
+        
+    except ValidationError as e:
+        print(e)
+        
+        print("---------")
+        print(e.absolute_path)
+    
+        print("---------")
+        print(e.absolute_schema_path)
 
 def check_connection(mongo_client, host, port):
     try:
